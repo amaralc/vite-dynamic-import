2a682f01b26650318c89aa143c627470a0d9a110
amaralc <amaral.calil@gmail.com>
2024-08-29 06:44:59 -0300

    fix: replace timeout by link and customize chunks

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
+
+
+const deriveFileNameFromChunkInfo = (chunkInfo) => {
+  if (!chunkInfo.facadeModuleId) {
+    return 'assets/js/[name]-[hash].js';
+  }
+
+  const basename = path.basename(chunkInfo.facadeModuleId);
+  if (basename.startsWith('@')) {
+    return 'assets/js/[name]-[hash].js';
+  }
+
+  const ext = path.extname(chunkInfo.facadeModuleId);
+  const outputFileName = chunkInfo.facadeModuleId.replace(__dirname, 'assets/js').replace(ext, '.js');
+  return outputFileName;
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: deriveFileNameFromChunkInfo,
+        entryFileNames: deriveFileNameFromChunkInfo,
+        assetFileNames: 'assets/[ext]/[name].[ext]',
+      },
+    },
+  },
 })
