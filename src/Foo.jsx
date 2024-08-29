8e57f881f8ac20a411cfd9741d4b64ea4f6f8751
amaralc <amaral.calil@gmail.com>
2024-08-29 06:54:09 -0300

    feat: add link back to home

--- src/Foo.jsx
+++ src/Foo.jsx
@@ -1,3 +1,12 @@
-const Foo = () => <p>Foo V1</p>;
+import { Link } from "react-router-dom";
+
+const Foo = () => {
+  return (
+    <div>
+      <p>Foo V1</p>
+      <Link to="/">Back to Home</Link>
+    </div>
+  );
+};
 
 export default Foo;
