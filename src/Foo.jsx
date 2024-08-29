27c3ce7e9b50cae715cbcd3aeb3df2d8fb499f3e
amaralc <amaral.calil@gmail.com>
2024-08-29 07:02:32 -0300

    Revert "Revert "Revert "debug: remove foo"""
    This reverts commit 7b79218fb320c8541d32824d262de870e69b0066.

---
+++ src/Foo.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Foo = () => {
+  return (
+    <div>
+      <p>Foo V1</p>
+      <Link to="/">Back to Home</Link>
+    </div>
+  );
+};
+
+export default Foo;
