e0b15960ec5c61603a8058f2f30549431ab8dc05
amaralc <amaral.calil@gmail.com>
2024-08-29 23:56:20 -0300

    debug: foo v2

--- src/Foo.tsx
+++ src/Foo.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Foo = () => {
   return (
     <div>
-      <p>Foo V1</p>
+      <p>Foo V2</p>
       <Link to="/">Back to Home</Link>
     </div>
   );
