c946abe62b8cce08047b52652218cdc898a87074
amaralc <amaral.calil@gmail.com>
2024-08-29 23:48:07 -0300

    chore: adujust ts error

--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import React from "react";
 
 interface ErrorBoundaryState {
   hasError: boolean;
 }
 
-class ErrorBoundary extends React.Component<React.PropsWithChildren<{}>, ErrorBoundaryState> {
+class ErrorBoundary extends React.Component<
+  React.PropsWithChildren<{}>,
+  ErrorBoundaryState
+> {
   constructor(props: React.PropsWithChildren<{}>) {
     super(props);
     this.state = { hasError: false };
   }
 
   static getDerivedStateFromError(error: Error) {
+    console.log(error);
     return { hasError: true };
   }
 
   componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-    console.error('Error caught by ErrorBoundary:', error, errorInfo);
+    console.error("Error caught by ErrorBoundary:", error, errorInfo);
   }
 
   render() {
     if (this.state.hasError) {
-        window.location.reload()
+      window.location.reload();
       return <h1>Something went wrong. Module not found (404).</h1>;
     }
 
