3e570de511d18131e4ca89846337d125d0cedd4e
amaralc <amaral.calil@gmail.com>
2024-08-29 23:45:59 -0300

    fix: update app to use error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { browserRouter } from "./AppRouter";
-import { ErrorBoundary } from "./ErrorBoundary";
+import ErrorBoundary from "./ErrorBoundary";
 
 function App() {
   return (
-    <ErrorBoundary fallback={<div>Error Caught by Top Level Error Boundary</div>} onError={(error, errorInfo) => console.log(error, errorInfo)}>
+    <ErrorBoundary>
       <Suspense fallback={<div>Loading...</div>}>
         <RouterProvider router={browserRouter} />
       </Suspense>
