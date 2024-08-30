1f6283924d984e20ee5ae4a54891f00a529ef389
amaralc <amaral.calil@gmail.com>
2024-08-29 23:53:19 -0300

    debug: use hashes in chunk names

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'node:path'
+// import path from 'node:path'
 
 
-const deriveFileNameFromChunkInfo = (chunkInfo: { facadeModuleId: string | null }): string => {
-  if (!chunkInfo.facadeModuleId) {
-    return 'assets/js/[name]-[hash].js';
-  }
+// const deriveFileNameFromChunkInfo = (chunkInfo: { facadeModuleId: string | null }): string => {
+//   if (!chunkInfo.facadeModuleId) {
+//     return 'assets/js/[name]-[hash].js';
+//   }
 
-  const basename = path.basename(chunkInfo.facadeModuleId);
-  if (basename.startsWith('@')) {
-    return 'assets/js/[name]-[hash].js';
-  }
+//   const basename = path.basename(chunkInfo.facadeModuleId);
+//   if (basename.startsWith('@')) {
+//     return 'assets/js/[name]-[hash].js';
+//   }
 
-  const ext = path.extname(chunkInfo.facadeModuleId);
-  const outputFileName = chunkInfo.facadeModuleId.replace(__dirname, 'assets/js').replace(ext, '.js');
-  return outputFileName;
-};
+//   const ext = path.extname(chunkInfo.facadeModuleId);
+//   const outputFileName = chunkInfo.facadeModuleId.replace(__dirname, 'assets/js').replace(ext, '.js');
+//   return outputFileName;
+// };
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        chunkFileNames: deriveFileNameFromChunkInfo,
-        entryFileNames: deriveFileNameFromChunkInfo,
-        assetFileNames: 'assets/[ext]/[name].[ext]',
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       chunkFileNames: deriveFileNameFromChunkInfo,
+  //       entryFileNames: deriveFileNameFromChunkInfo,
+  //       assetFileNames: 'assets/[ext]/[name].[ext]',
+  //     },
+  //   },
+  // },
 })
